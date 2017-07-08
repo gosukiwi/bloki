89bf28318d67b59b20aee5c8f85eb188015271de
Federico Ramirez <fedra.arg@gmail.com>
2017-07-08 15:43:40 -0300

    Tidy up

--- lib/parser.lisp
+++ lib/parser.lisp
@@ -2,6 +2,9 @@
 ;; Below is a simple wrapper using strings.
 ;; This is just for development purposes, ideally this would use some kind of stream
 
+(defun make-pinput (char)
+  (coerce (list char) 'string))
+
 (defun peek (input)
   "peeks some input, returns the first character and the rest"
   (let ((head (elt input 0))
@@ -13,26 +16,45 @@
       input
       (list input)))
 
-(defun concatenate-input (a b)
+(defun pinput-concat (a b)
   (if a
       (if b
           (concatenate 'string (stringify a) (stringify b))
           a)
       b))
 
-;; Utility functions for parsers
-(defun parser-ok (matched rest)
-  (list t matched rest))
+;; Parser result
+(defstruct presult
+  (success   nil :type boolean)
+  (matched   nil :type string)
+  (remaining nil :type string))
+;; TODO :type should be pinput rather than string
 
-(defun parser-fail (input)
-  (list nil nil input))
+;; Utility functions for parsers
+(defun presult-ok (matched rest)
+  (make-presult :success t :matched matched :remaining rest))
+
+(defun presult-fail (input)
+  (make-presult :success nil :matched "" :remaining input))
+
+;; results look like this: '(t #\f "oo")
+;;                         '(nil nil "foo")
+(defun presult-concat (a b)
+  (if (presult-success b)
+      (if (presult-success a)
+          (let ((result (pinput-concat (presult-matched a) (presult-matched b))))
+            (presult-ok result (presult-remaining b)))
+          b)
+      a))
 
 (defmacro defp (name args body)
-  "Sugar for making parsers. In the parser body you'll have access to the variables: head, tail, and input."
+  "Sugar for making parsers. In the parser body you'll have access to the variables:
+   head-as-char, head, tail, and input."
   `(defun ,name ,args
      (lambda (input)
-       (multiple-value-bind (head tail) (peek input)
-         (progn ,body)))))
+       (multiple-value-bind (head-as-char tail) (peek input)
+         (let ((head (make-pinput head-as-char)))
+           ,body)))))
 
 (defun run-parser (parser input)
   "Helper function to run parsers on a given input"
@@ -45,14 +67,14 @@
 ;; takes a string of possible characters to pick from
 (defp any-of (chars)
   (loop named char-loop for char in (coerce chars 'list)
-        finally (return-from char-loop (parser-fail input)) do
-          (if (equalp char head)
-              (return-from char-loop (parser-ok head tail)))))
+        when (equalp char head-as-char)
+          do (return-from char-loop (presult-ok head tail))
+        finally (return-from char-loop (presult-fail input))))
 
 (defp one (char)
-  (if (equalp char head)
-      (parser-ok head tail)
-      (parser-fail input)))
+  (if (equalp char head-as-char)
+      (presult-ok head tail)
+      (presult-fail input)))
 
 (defp whitespace ()
   (run-parser (any-of '(#\Space #\Backspace #\Linefeed #\Tab #\Return)) input))
@@ -60,31 +82,18 @@
 ;; Unary parser combinators
 ;; They take a parser and return another parser
 
-(defun subseq-input (input index)
-  (subseq input index))
-
-;; results look like this: '(t #\f "oo")
-;;                         '(nil nil "foo")
-(defun concatenate-presults (a b)
-  (if (car b)
-      (if (car a)
-          (let ((result (concatenate-input (nth 1 a) (nth 1 b))))
-            (list t result (nth 2 b)))
-          b)
-      a))
-
 ;; matches something one or more times
 (defun many-1 (parser)
   (lambda (input)
-    (let ((matched (list nil nil input))
+    (let ((matched (presult-fail input))
           (remaining input)
           (iterating t))
       (loop for new-matched = (run-parser parser remaining)
             while iterating
             do (cond
-                 ((car new-matched)
-                  (setq matched (concatenate-presults matched new-matched))
-                  (setq remaining (nth 2 new-matched)))
+                 ((presult-success new-matched)
+                  (setq matched (presult-concat matched new-matched))
+                  (setq remaining (presult-remaining new-matched)))
                  (t
                   (setq iterating nil)))
             finally (return matched)))))
