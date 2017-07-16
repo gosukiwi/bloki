2dbab0a08a2dfc9bc13059dc25d290063befb33e
Federico Ramirez <fedra.arg@gmail.com>
2017-07-16 14:24:40 -0300

    More silly tests

--- test/parser-tests.lisp
+++ test/parser-tests.lisp
@@ -11,11 +11,15 @@
   (ok (presult-success (test-parser parser input)) ))
 
 ;; try simple blocks
-(it-parses (pblock) "[1 + 2]")
+(it-parses (pblock) "1")
+(it-parses (pblock) "\"hi\"")
 (it-parses (pblock) "[foo 1]")
+(it-parses (pblock) "[1 + 2]")
 (it-parses (pblock) "[foo \"hi\"]")
 (it-parses (pblock) "[foo :bar \"hi\"]")
 (it-parses (pblock) "[foo :bar \"hi\" :baz 2]")
 (it-parses (pblock) "[[foo 1] + 2]")
+(it-parses (pblock) "[[foo 1] + [foo 1]]")
+(it-parses (pblock) "[[foo 1] + [2 - 1]]")
 
 (finalize)
